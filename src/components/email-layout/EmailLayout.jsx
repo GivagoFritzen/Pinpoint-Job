d37c242de303f47cb8ae4d6eae27e7b64d2f4e29
Givago Fritzen <givago.fritzen@cwi.com.br>
2019-03-21 14:38:18 -0300

    Pegando dadas antigas a 3 meses

--- src/components/email-layout/EmailLayout.jsx
+++ src/components/email-layout/EmailLayout.jsx
@@ -5,16 +5,20 @@ import Grid from './Grid';
 export class EmailLayout extends Component {
     static propTypes = {
         month: PropTypes.string.isRequired,
+        schedules: PropTypes.oneOf([
+            PropTypes.array,
+            PropTypes.string
+        ]).isRequired,
     }
 
     render() {
-        const { month } = this.props
+        const { month, schedules } = this.props
 
         return (
             <Grid>
                 <h1>Informações do Mês {month}</h1>
                 <Grid.Row>
-                    <p>Teste</p>
+                    {schedules}
                 </Grid.Row>
             </Grid>
         );
--- src/scenes/eletronicPoint/EletronicPoint.jsx
+++ src/scenes/eletronicPoint/EletronicPoint.jsx
@@ -7,11 +7,13 @@ import * as emailjs from 'emailjs-com';
 import './EletronicPoint.scss'
 
 import Fingersprint from '../../images/Fingerprint.svg'
+import { EmailLayout } from '../../components/email-layout/EmailLayout';
 
 export class EletronicPoint extends Component {
 
     componentDidMount() {
         this._sendEmail();
+        this._getMarkPoint()
     }
 
     _getMarkPoint() {
@@ -20,49 +22,65 @@ export class EletronicPoint extends Component {
         }
         else {
             /* Var's */
-            var db;
-            var objectStore;
-            var request;
-            var transaction;
+            let db;
+            let objectStore;
+            let request;
+            let transaction;
 
             request = window.indexedDB.open("Ponto", 1);
+            request.onerror = function () {
+                toast.error("Erro ao abrir o banco de dados ");
+            }
+
+            request.onupgradeneeded = function (event) {
+                toast.success("Atualizando...");
+                db = event.target.result;
+                objectStore = db.createObjectStore("Ponto", { keyPath: "id", autoIncrement: true });
+                db.close();
+            };
+
             request.onsuccess = function (event) {
                 const timetable = document.getElementById('timetable');
                 if (timetable.childNodes.length === 1) {
+                    try {
+                        db = event.target.result;
+                        transaction = db.transaction(["Ponto"], 'readwrite');
+                        objectStore = transaction.objectStore("Ponto");
+                        request = objectStore.getAll("Ponto");
 
-                    db = event.target.result;
-                    transaction = db.transaction(["Ponto"], "readwrite");
-                    objectStore = transaction.objectStore("Ponto");
-                    request = objectStore.getAll("Ponto");
+                        let dates = []
+                        let lastDate = ''
+                        let timeTableTR
 
-                    let dates = []
-                    let lastDate = ''
-                    let timeTableTR
+                        objectStore.openCursor().onsuccess = function (event) {
+                            const cursor = event.target.result;
+                            if (cursor) {
+                                const date = moment(cursor.value).format('DD/M/YYYY, H:mm:ss');
 
-                    objectStore.openCursor().onsuccess = function (event) {
-                        const cursor = event.target.result;
-                        if (cursor) {
-                            const date = moment(cursor.value).format('DD/M/YYYY, H:mm:ss');
+                                if (lastDate === '' || date.substring(0, 2) !== lastDate.substring(0, 2)) {
+                                    timeTableTR = document.createElement('tr');
+                                    timetable.appendChild(timeTableTR);
+                                    lastDate = date;
+                                }
 
-                            if (lastDate === '' || date.substring(0, 2) !== lastDate.substring(0, 2)) {
-                                timeTableTR = document.createElement('tr');
+                                dates.push(cursor.value);
+
+                                let timeTableTD = document.createElement('td');
+                                timeTableTD.innerHTML += date
+                                timeTableTR.appendChild(timeTableTD);
+
+                                cursor.continue();
+
                                 timetable.appendChild(timeTableTR);
-                                lastDate = date;
                             }
-
-                            dates.push(cursor.value);
-
-                            let timeTableTD = document.createElement('td');
-                            timeTableTD.innerHTML += date
-                            timeTableTR.appendChild(timeTableTD);
-
-                            cursor.continue();
-
-                            timetable.appendChild(timeTableTR);
                         }
                     }
-                }
-            };
+                    catch (event) {
+                        toast.error("Erro:" + event);
+                    }
+                };
+
+            }
         }
     }
 
@@ -72,10 +90,10 @@ export class EletronicPoint extends Component {
         }
         else {
             /* Var's */
-            var db;
-            var objectStore;
-            var request;
-            var transaction;
+            let db;
+            let objectStore;
+            let request;
+            let transaction;
 
             request = window.indexedDB.open("Ponto", 1);
             request.onerror = function () {
@@ -94,6 +112,15 @@ export class EletronicPoint extends Component {
                     db = event.target.result;
                     transaction = db.transaction(["Ponto"], 'readwrite');
                     objectStore = transaction.objectStore("Ponto", { keyPath: "id", autoIncrement: true });
+
+                    //Test                    
+                    var x = new Date();
+                    x.setDate(1);
+                    x.setMonth(x.getMonth() - 4);
+                    objectStore.add(
+                        x
+                    );
+
                     objectStore.add(
                         new Date()
                     );
@@ -107,24 +134,89 @@ export class EletronicPoint extends Component {
                 }
             }
         }
+
+        window.location.reload();
     }
 
     _sendEmail() {
-        var template_params = {
-            "reply_to": "to",
-            "from_name": "from",
-            "to_name": "Sujeito",
-            "message_html": "Uma mensagem"
-        }
+        const date = new Date()
+        const currentMonth = date.getMonth() + 1
 
-        var service_id = "";
-        var template_id = "";
-        var user_id = '';
-        emailjs.send(service_id, template_id, template_params, user_id).then(response => {
-            toast.success('Email enviado com sucesso');
-        }).catch((error) => {
-            toast.error("Erro ao enviar email: " + error);
-        })
+        let controller = false;
+        let dates = []
+
+        if (window.indexedDB) {
+            /* Var's */
+            let db;
+            let objectStore;
+            let request;
+            let transaction;
+
+            request = window.indexedDB.open("Ponto", 1);
+            request.onerror = function () {
+                return;
+            }
+
+            request.onsuccess = function (event) {
+                try {
+                    db = event.target.result;
+                    transaction = db.transaction(["Ponto"], 'readwrite');
+                    objectStore = transaction.objectStore("Ponto");
+                    request = objectStore.getAll("Ponto");
+
+                    objectStore.openCursor().onsuccess = function (event) {
+                        const cursor = event.target.result;
+
+                        if (cursor && !controller) {
+                            const date = moment(cursor.value).format('DD/MM/YYYY, H:mm:ss');
+                            const month = date.slice(3, 5);
+
+                            if (currentMonth <= 3) {
+                                switch (month) {
+                                    case '12':
+                                    case '11':
+                                    case '10':
+                                        dates.push(date);
+                                        cursor.delete();
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            else if ((currentMonth - month) <= 3) {
+                                dates.push(date);
+                                cursor.delete();
+                            }
+                            else {
+                                controller = false;
+                            }
+                            cursor.continue();
+                        }
+                        else if (dates.length > 0) {
+                            const month = dates[0].slice(3, 5);
+                            var template_params = {
+                                "reply_to": "to",
+                                "from_name": "from",
+                                "to_name": "Sujeito",
+                                "message_html": <EmailLayout month={month} schedules={dates.toString()} />
+                            }
+
+                            var service_id = 'mailjet';
+                            var template_id = 'template_id';
+                            var user_id = 'user_id';
+                            emailjs.send(service_id, template_id, template_params, user_id).then(response => {
+                                toast.success('Email enviado com sucesso');
+                            }).catch((error) => {
+                                toast.error("Erro ao enviar email: " + error);
+                            })
+                        }
+                    }
+                }
+                catch (event) {
+                    return;
+                }
+            }
+        }
     }
 
     render() {
@@ -139,6 +231,7 @@ export class EletronicPoint extends Component {
                     rtl={false}
                     pauseOnVisibilityChange
                     draggable
+
                     pauseOnHover
                 />
                 <h1>Ponto</h1>
@@ -153,7 +246,6 @@ export class EletronicPoint extends Component {
     }
 
     renderTimeTable() {
-        this._getMarkPoint()
         return (
             <table>
                 <tbody id='timetable'>
